d9c0c70e9fc74ff75dbf7720fe134d48387b07de
Matej Poklemba <matejpoklemba@gmail.com>
2022-02-21 00:55:30 +0100

    Rewrite stuff for avax

--- contracts/interfaces/IWAVAX.sol
+++ contracts/interfaces/IWAVAX.sol
@@ -1,7 +1,8 @@
 pragma solidity >=0.5.0;
 
-interface IWETH {
+interface IWAVAX {
     function deposit() external payable;
     function transfer(address to, uint value) external returns (bool);
+    function balanceOf(address owner) external view returns (uint);
     function withdraw(uint) external;
 }
